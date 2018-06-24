88c7c1cf0e3ea70caeaccb3fef139fdda0c8a1c7
Richard Armuelles <alexanderarmuelles@riseup.net>
2018-06-24 16:56:54 -0500

    a simple beforeinstallprompt

--- public/javascripts/sw.js
+++ public/javascripts/sw.js
@@ -14,32 +14,13 @@ var filesToCache = [
   '/javascripts/getrecipe-form.js'
 ];
 
-var installPromptEvent;
+let deferredPrompt;
 
-self.addEventListener('beforeinstallprompt', function(e){
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
   // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
-});
-
-document.getElementById('install').addEventListener('click', function(){
-  // Update the install UI to remove the install button
-  document.getElementById('install').disabled = true;
-  // Show the modal add to home screen dialog
-  installPromptEvent.prompt();
-  // Wait for the user to respond to the prompt
-  installPromptEvent.userChoice.then(function(choice) {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the saved prompt since it can't be used again
-    installPromptEvent = null;
-  });
+  deferredPrompt = e;
 });
 
 self.addEventListener('install', function(e) {
