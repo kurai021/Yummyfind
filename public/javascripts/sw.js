88045383a8f70ad7ba6bbc51dc2209998dc25fcd
Richard Armuelles <alexanderarmuelles@riseup.net>
2018-07-02 15:26:41 -0500

    update cache files

--- public/javascripts/sw.js
+++ public/javascripts/sw.js
@@ -1,12 +1,22 @@
 //This is the "Offline page" service worker
 //Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
-  var offlinePage = new Request('/offline');
+  //var offlinePage = new Request('/offline');
   event.waitUntil(
     fetch(offlinePage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
         console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-        return cache.put(offlinePage, response);
+        //return cache.put(offlinePage, response);
+        return cache.addAll(
+          [
+            '/offline',
+            '/client/materialize-css/dist/css/materialize.min.css',
+            '/stylesheets/style.css',
+            '/client/jquery/dist/jquery.min.js',
+            '/client/materialize-css/dist/js/materialize.min.js',
+            '/javascripts/init-offline.js'
+          ]
+        );
       });
   }));
 });
