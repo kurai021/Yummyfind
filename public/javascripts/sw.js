1884c3d3a113ce19dd8cca7dc1622c2d4161db7d
Richard Armuelles <alexanderarmuelles@riseup.net>
2018-07-03 09:37:21 -0500

    testing new sw

--- public/javascripts/sw.js
+++ public/javascripts/sw.js
@@ -1,64 +1,162 @@
-var cacheName = 'YummyFind';
-var filesToCache = [
-  '/',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  '/client/materialize-css/dist/css/materialize.min.css',
-  '/stylesheets/style.css',
-  '/client/jquery/dist/jquery.min.js',
-  '/client/materialize-css/dist/js/materialize.min.js',
-  '/client/handlebars/dist/handlebars.min.js',
-  '/socket.io/socket.io.js',
-  '/javascripts/init.js',
-  '/javascripts/video.js',
-  '/javascripts/getrecipe-socket.js',
-  '/javascripts/getrecipe-form.js'
-];
+const config = {
+    version: 'v0.10',
+    precache_urls: [
+      'https://fonts.googleapis.com/icon?family=Material+Icons',
+      '/client/materialize-css/dist/css/materialize.min.css',
+      '/stylesheets/style.css',
+      '/client/jquery/dist/jquery.min.js',
+      '/client/materialize-css/dist/js/materialize.min.js',
+      '/client/handlebars/dist/handlebars.min.js',
+      '/socket.io/socket.io.js',
+      '/javascripts/init.js',
+      '/javascripts/video.js',
+      '/javascripts/getrecipe-socket.js',
+      '/javascripts/getrecipe-form.js'
+    ]
+  },
+  preCacheName = "precache-" + config.version,
+  preCacheNoDependencyName = "precache-no-dependency-" + config.version,
+  dynamicCacheName = "dynamic-cache-" + config.version;
 
-let deferredPrompt;
+function cacheName(key, opts) {
+  return key + "-" + opts.version;
+}
 
-self.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  btnAdd.style.display = 'block';
+//abstract the add to cache functionality to a common function
+function addToCache(cacheKey, request, response) {
 
-  btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+  //only cache good responses (200)
+  if (response.ok) {
+
+    var copy = response.clone();
+
+    caches.open(cacheKey).then(cache => {
+      cache.put(request, copy);
     });
+
+  }
+
+  return response;
+
+}
+
+//abstract the fetching from cache functionality to a common function
+function fetchFromCache(event) {
+
+  return caches.match(event.request).then(response => {
+
+    if (!response) {
+      //fall back to the network fetch here
+      throw Error(`${event.request.url} not found in cache`);
+    }
+
+    return response;
+
   });
+}
+
+//offline fallback functionality
+function offlineResponse(resourceType, opts) {
+
+  if (resourceType === 'image' || resourceType === 'product-img') {
+
+    //return custom response with a placeholder image
+    return new Response(opts.offlineImage, {
+      headers: {
+        'Content-Type': 'image/svg+xml'
+      }
+    });
+
+  } else if (resourceType === 'content' || resourceType === 'product') {
+    return caches.match(opts.offlinePage);
+  }
+
+  //can't do a common fallback for other file types like css & JavaScript
+  return undefined;
+}
+
+function getURL(event) {
+
+  return new URL(event.request.url);
+
+}
+
+//copied froma Jeremy Kieth article - https://adactio.com/journal/9888
+//common method to reduce the amount of records cached so you don't
+//bloat the customer's hard drive.
+//remember many smart phone have limited space available and we don't
+//have a good way to know if we are consuming too much.
+function trimCache(cacheName, maxItems) {
+
+  caches.open(cacheName)
+    .then(function (cache) {
+      cache.keys()
+        .then(function (keys) {
+          if (keys.length > maxItems) {
+
+            cache.delete(keys[0])
+              //recursively call trimCache function until we have the max # of items cached
+              .then(trimCache(cacheName, maxItems));
+          }
+        });
+    });
+
+};
+
+self.addEventListener("install", event => {
+
+  caches.open(preCacheName).then(cache => {
+
+    return cache.addAll(preCacheFiles);
+
+  });
+
+
 });
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+self.addEventListener("activate", event => {
+
+  //on activate
+  event.waitUntil(caches.keys()
+    .then(function (cacheNames) {
+
+      cacheNames.forEach(value => {
+
+        if (value.indexOf(config.version) < 0) {
+
+          caches.delete(value);
+
+        }
+
+      });
+
+      return;
+
     })
   );
+
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", function (event) {
+
+  //  return fetch(event.request);
+
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
+
+    caches.match(event.request).then(response => {
+
+      if (!response) {
+
+        //fall back to the network fetch
+        return fetch(event.request);
+
+      }
+
+      return response;
+
     })
-  );
+
+  )
+
 });
